be938c359cd9066cf8ffc3a1ad2561529b4d1179
cyberadryn <ajchung15@gmail.com>
2024-11-24 16:06:17 -0500

    Updated Pokemon API

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ from imgcat import imgcat
 from PIL import Image
 from io import BytesIO
 
+# Base Url
 base_url = "https://pokeapi.co/api/v2/"
 
 def get_pokemon_info(name):
     url = f"{base_url}/pokemon/{name}"
     response = requests.get(url)
-   
    
     if response.status_code == 200:
         pokemon_data = response.json()
@@ -18,24 +18,19 @@ def get_pokemon_info(name):
     else:
         print(f"Failed to retrieve date {response.status_code}")
 
+# Prompt for Pokemon Name
 pokemon_name = input("Enter Pokemon name: ").lower()
-# pokemon_name = 'cinderace'
 pokemon_info = get_pokemon_info(pokemon_name)
-
-
-gen_url='https://pokeapi.co/api/v2/pokemon-species/'
-def generation(gen):
-    url = f"{base_url}//pokemon-species/{gen}"
-    response = requests.get(gen_url)
-pokemon_info2 = generation(pokemon_name)
-
 
 if pokemon_info:
    print("\n")
-#    print("\nSprite URL: " + pokemon_info["sprites"]["front_default"])  # Access sprite image URL
+
+   # Pokemon Image
    response = requests.get(pokemon_info["sprites"]["front_default"])
    img = Image.open(BytesIO(response.content))
    imgcat(img)
+
+   # Pokemon Info
    print ("Name: " + f"{pokemon_info["name"].capitalize()}")
    print ("ID: "f"{pokemon_info["id"]}")
    print ("Height: " f"{pokemon_info["height"]}") 
